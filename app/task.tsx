2e9bef3d40cc2b5bd20430f9809a3178b1102a4a
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-05-16 04:22:43 -0400

    Added cycling through tasks

--- app/task.tsx
+++ app/task.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
+"use client";
+import React, { FC, useState } from "react";
 import jsonData from "../app/data.json";
-
 interface Task {
   title: string;
   description: string;
@@ -11,28 +11,59 @@ interface CardProps {
   tasks: Task[];
 }
 
-const Buttons = () => {
+interface ButtonsProps {
+  onIncrement: () => void;
+  onDecrement: () => void;
+}
+
+const Buttons: FC<ButtonsProps> = ({ onIncrement, onDecrement }) => {
   return (
     <div className="flex">
-      <button className="b-4 m-4 bg-green-600 px-12 py-6 text-xl font-bold text-white">
-        Accept
+      <button
+        onClick={onDecrement}
+        className="b-4 m-4 bg-red-400 px-12 py-6 text-xl font-bold text-white"
+      >
+        Previous
       </button>
-      <button className="b-4 m-4 bg-red-600 px-12 py-6 text-xl font-bold text-white">
-        Decline
+      <button
+        onClick={onIncrement}
+        className="b-4 m-4 bg-green-400 px-12 py-6 text-xl font-bold text-white"
+      >
+        Next
       </button>
     </div>
   );
 };
 
 const Card: FC<CardProps> = ({ tasks }) => {
-  const { title, description, date } = tasks[1];
+  const [count, setCount] = useState(0);
+  const { title, description, date } = tasks[count];
+
+  const increment = () => {
+    if (count === tasks.length - 1) {
+      setCount(0);
+      return;
+    }
+    setCount((count) => count + 1);
+  };
+
+  const decrement = () => {
+    if (count === 0) {
+      setCount(tasks.length - 1);
+      return;
+    }
+    setCount((count) => count - 1);
+  };
 
   return (
     <div className="flex h-5/6 w-2/3 flex-col items-center bg-gray-600 p-4">
       <h1 className="p-5 text-6xl">{title}</h1>
       <p>{description}</p>
       <p>{date}</p>
-      <Buttons />
+      <div className="flex-grow" />
+      <div className="mt-auto">
+        <Buttons onIncrement={increment} onDecrement={decrement} />
+      </div>
     </div>
   );
 };
