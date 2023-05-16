765103e323ca4c43d4065033440f753ca3753afa
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-05-16 01:13:32 -0400

    Added basic json functionality

--- app/task.tsx
+++ app/task.tsx
@@ -1,15 +1,25 @@
 import React, { FC } from "react";
+import jsonData from "../app/data.json";
 
 interface CardProps {
-  title: string;
-  description: string;
-  date: string;
+  jsonData: {
+    task: {
+      title: string;
+      description: string;
+      date: string;
+    }[];
+  };
 }
-const Card: FC<CardProps> = ({ title, description, date }) => {
+
+const Card: FC<CardProps> = ({ jsonData }) => {
+  const { task } = jsonData;
+  const { title, description, date } = task[0];
+
   return (
     <div className="flex h-5/6 w-2/3 flex-col items-center bg-gray-600 p-4">
       <h1 className="p-5 text-6xl">{title}</h1>
       <p>{description}</p>
+      <p>{date}</p>
     </div>
   );
 };
@@ -17,11 +27,7 @@ const Card: FC<CardProps> = ({ title, description, date }) => {
 const Task: FC = () => {
   return (
     <div className="flex min-h-screen grow items-center justify-center bg-gray-500">
-      <Card
-        title="Task Title"
-        description="Task Description"
-        date="Task Date"
-      />
+      <Card jsonData={jsonData} />
     </div>
   );
 };
