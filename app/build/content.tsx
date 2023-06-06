9de903dcf34131c037f20ba3f083a85a2f2305a6
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-06-06 05:05:50 -0400

    added build page

---
+++ app/build/content.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+const Content: FC = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-800">
+      hi
+    </div>
+  );
+};
+export default Content;
