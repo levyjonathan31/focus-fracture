de6df57c924ca203e27622725596ec1134e29662
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-06-06 05:34:37 -0400

    removed xp bar

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,14 +1,20 @@
 import React, { FC } from "react";
-
 const Sidebar: FC = () => {
   return (
     <div className="flex min-h-screen w-1/6 flex-col items-center bg-gray-700">
-      <h1 className="w-full border-b-2 border-black p-4 text-center text-3xl text-purple-500">
+      <a
+        href="/"
+        className="w-full border-b-2 border-black p-4 text-center text-3xl text-purple-500"
+      >
         Focus Fracture
-      </h1>
-      <button className="m-4 w-3/5 rounded-md bg-gray-800 p-4 text-white">
-        Start
-      </button>
+      </a>
+      <a
+        href="/build"
+        className="mt-4 w-3/5 rounded-md bg-gray-800 p-4 text-center text-white"
+      >
+        Build Task
+      </a>
+      <div className="grow"></div>
     </div>
   );
 };
