baf54a8a1a8dc20db757a0494ed1f874e684f4a0
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-06-08 12:19:45 -0400

    fixed text wrapping on sidebar

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,25 +1,25 @@
 import React, { FC } from "react";
+const Button: FC<{ title: string }> = ({ title }) => {
+  return (
+    <a
+      href="/"
+      className="mt-4 w-3/5 rounded-md bg-gray-800 p-4 text-center text-white"
+    >
+      {title}
+    </a>
+  );
+};
 const Sidebar: FC = () => {
   return (
-    <div className="flex min-h-screen w-1/6 flex-col items-center bg-gray-700 text-xl">
+    <div className="flex min-h-screen w-1/6 flex-col items-center break-words bg-gray-700 text-xl">
       <a
         href="/"
         className="w-full border-b-2 border-black p-4 text-center text-4xl text-purple-500"
       >
         Focus Fracture
       </a>
-      <a
-        href="/"
-        className="mt-4 w-3/5 rounded-md bg-gray-800 p-4 text-center text-white"
-      >
-        Workspace
-      </a>
-      <a
-        href="/build"
-        className="mt-4 w-3/5 rounded-md bg-gray-800 p-4 text-center text-white"
-      >
-        Build Task
-      </a>
+      <Button title="Workspace" />
+      <Button title="Build Task" />
       <div className="grow"></div>
     </div>
   );
