ba95462ac9f4ed43ac0dca186b490a519a667bb5
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-06-11 05:27:01 -0400

    Added date reorganized states

--- app/build/task-build.tsx
+++ app/build/task-build.tsx
@@ -1,17 +1,24 @@
 "use client";
+import { title } from "process";
 import React, { FC, useState } from "react";
-const Shard: FC<{ label: string }> = ({ label }) => {
-  const [title, setTitle] = useState("");
+
+const TaskInput: FC<{
+  label: string;
+  kind: string;
+  data: string;
+  setFunction: React.Dispatch<React.SetStateAction<string>>;
+}> = ({ label, kind, data, setFunction }) => {
+  console.log(title);
   return (
     <div className="flex flex-grow flex-col items-center">
       <label className="m-2">{label}</label>
       <input
-        size={25}
-        type="text"
+        size={17}
+        type={kind}
         className="mb-8 resize-none rounded-md bg-gray-700 text-center"
         name="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
+        value={data}
+        onChange={(e) => setFunction(e.target.value)}
         required
       />
     </div>
@@ -19,17 +26,53 @@ const Shard: FC<{ label: string }> = ({ label }) => {
 };
 
 const TaskForm: FC = () => {
+  const [name, setName] = useState("");
+  const [shard1, setShard1] = useState("");
+  const [shard2, setShard2] = useState("");
+  const [shard3, setShard3] = useState("");
+  const [date, setDate] = useState("");
+
   function onSubmit(event: React.FormEvent) {
-    console.log("onSubmit");
+    console.log(name);
+    console.log(shard1);
+    console.log(shard2);
+    console.log(shard3);
+    console.log(date);
   }
   return (
-    <form noValidate onSubmit={onSubmit} className="text-center text-2xl">
-      <Shard label="Title" />
+    <form
+      method="post"
+      noValidate
+      onSubmit={onSubmit}
+      className="text-center text-2xl"
+    >
+      <TaskInput label="Title" kind="text" data={name} setFunction={setName} />
       <div className="flex">
-        <Shard label="Shard" />
-        <Shard label="Shard" />
-        <Shard label="Shard" />
+        <TaskInput
+          label="Shard"
+          kind="text"
+          data={shard1}
+          setFunction={setShard1}
+        />
+        <TaskInput
+          label="Shard"
+          kind="text"
+          data={shard2}
+          setFunction={setShard2}
+        />
+        <TaskInput
+          label="Shard"
+          kind="text"
+          data={shard3}
+          setFunction={setShard3}
+        />
       </div>
+      <TaskInput
+        label="Target Date"
+        kind="date"
+        data={date}
+        setFunction={setDate}
+      />
       <button
         type="submit"
         className="mb-4 w-1/6 rounded-md bg-gray-800 p-4 text-center"
