e26d07c59e0ad39da4d8a40454fa31ca1e14a2ec
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-06-17 04:48:16 -0400

    modifed task form

--- app/build/task-build.tsx
+++ app/build/task-build.tsx
@@ -4,18 +4,18 @@ import React, { FC, useState } from "react";
 
 const TaskInput: FC<{
   label: string;
-  kind: string;
   data: string;
   id: string;
+  kind: string;
   setFunction: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}> = ({ label, kind, data, id, setFunction }) => {
+}> = ({ label, data, id, kind, setFunction }) => {
   return (
     <div className="flex flex-grow flex-col items-center">
-      <label className="m-2">{label}</label>
+      <label className="">{label}</label>
       <input
         size={17}
+        className="mx-6 mb-4 resize-none rounded-md bg-gray-700 text-center"
         type={kind}
-        className="mb-8 resize-none rounded-md bg-gray-700 text-center"
         name={id}
         value={data}
         onChange={(e) => setFunction(e)}
@@ -33,21 +33,35 @@ const TaskForm: FC = () => {
     ("0" + (current.getMonth() + 1)).slice(-2) +
     "-" +
     ("0" + current.getDate()).slice(-2);
-  console.log(date);
   const [formInputs, setFormInputs] = useState({
     name: "",
-    shard1: "",
-    shard2: "",
-    shard3: "",
-    date: date,
+    shard1: {
+      description1: "",
+      category1: "",
+      measure1: "",
+      deadline1: date,
+    },
+    shard2: {
+      description2: "",
+      category2: "",
+      measure2: "",
+      deadline2: date,
+    },
   });
-
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    console.log(formInputs);
+
     setFormInputs((prevFormInputs) => ({
       ...prevFormInputs,
       [name]: value,
+      shard1: {
+        ...prevFormInputs.shard1,
+        [name]: value,
+      },
+      shard2: {
+        ...prevFormInputs.shard2,
+        [name]: value,
+      },
     }));
   };
 
@@ -78,41 +92,74 @@ const TaskForm: FC = () => {
     >
       <TaskInput
         label="Title"
-        kind="text"
         data={formInputs.name}
         id="name"
+        kind="text"
         setFunction={handleInputChange}
       />
-      <div className="flex">
-        <TaskInput
-          label="Shard"
-          kind="text"
-          data={formInputs.shard1}
-          id="shard1"
-          setFunction={handleInputChange}
-        />
-        <TaskInput
-          label="Shard"
-          kind="text"
-          data={formInputs.shard2}
-          id="shard2"
-          setFunction={handleInputChange}
-        />
-        <TaskInput
-          label="Shard"
-          kind="text"
-          data={formInputs.shard3}
-          id="shard3"
-          setFunction={handleInputChange}
-        />
+      <div className="flex justify-center">
+        <div className="flex flex-col">
+          <TaskInput
+            label="Description"
+            data={formInputs.shard1.description1}
+            id="description1"
+            kind="text"
+            setFunction={handleInputChange}
+          />
+          <TaskInput
+            label="Category"
+            data={formInputs.shard1.category1}
+            id="category1"
+            kind="text"
+            setFunction={handleInputChange}
+          />
+          <TaskInput
+            label="Measure"
+            data={formInputs.shard1.measure1}
+            id="measure1"
+            kind="text"
+            setFunction={handleInputChange}
+          />
+          <TaskInput
+            label="Target Date"
+            data={formInputs.shard1.deadline1}
+            id="deadline1"
+            kind="date"
+            setFunction={handleInputChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <TaskInput
+            label="Description"
+            data={formInputs.shard2.description2}
+            id="description2"
+            kind="text"
+            setFunction={handleInputChange}
+          />
+          <TaskInput
+            label="Category"
+            data={formInputs.shard2.category2}
+            id="category2"
+            kind="text"
+            setFunction={handleInputChange}
+          />
+          <TaskInput
+            label="Measure"
+            data={formInputs.shard2.measure2}
+            id="measure2"
+            kind="text"
+            setFunction={handleInputChange}
+          />
+          <TaskInput
+            label="Target Date"
+            data={formInputs.shard2.deadline2}
+            id="deadline2"
+            kind="date"
+            setFunction={handleInputChange}
+          />
+        </div>
       </div>
-      <TaskInput
-        label="Target Date"
-        kind="date"
-        data={formInputs.date}
-        id="date"
-        setFunction={handleInputChange}
-      />
+
       <button
         type="submit"
         className="mb-4 w-1/6 rounded-md bg-gray-800 p-4 text-center"
